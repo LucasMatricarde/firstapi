a42f423948bdec7ad3246ff1af227daa6d11ebc3
Lucas Matricarde <lucasmatricarde@hotmail.com>
2024-03-30 16:58:48 -0300

    Aprendendo node js

---
+++ src/controllers/UserController.js
@@ -0,0 +1,9 @@
+const users = require('../mocks/users');
+
+module.exports = {
+    listUsers(request, response) {
+        console.log(request.query);
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        response.end(JSON.stringify(users));
+    }
+};
---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        nome: 'Lucas',
+    },
+    {
+        id: 2,
+        nome: 'Rose',
+    },
+    {
+        id: 3,
+        nome: 'Jose',
+    },
+];
